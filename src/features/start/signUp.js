f0871de1422e3a32534938e8339ff201368a3c4b
okang1014 <okang1014@gmail.com>
2024-10-25 15:46:39 +0900

    style: 시작하기 회원가입 페이지 주석 추가

--- src/features/start/signUp.js
+++ src/features/start/signUp.js
@@ -2,44 +2,60 @@ import axios from 'axios';
 
 window.addEventListener('load', () => {
 
-  // 필요한 노드 획득
+  // 필요한 DOM 노드 획득
+  // 별명(이름) 영역 DOM 노드
   const $signUpNickname = document.getElementById('signUpNickname');
   const $checkNicknameBtn = document.getElementById('checkNickname');
   const $checkNicknameResult = document.querySelector('.sign-up__valid.nickname');
 
+  // 이메일 영역 DOM 노드
   const $signUpEmail = document.getElementById('signUpEmail');
   const $checkEmailBtn = document.getElementById('checkEmail');
   const $checkEmailResult = document.querySelector('.sign-up__valid.email');
 
+  // 비밀번호 영역 DOM 노드
   const $signUpPw = document.getElementById('signUpPw');
   const $signUpPwCheck = document.getElementById('signUpPwCheck');
-  const $checkPwResult = document.querySelector('.sign-up__valid.pw')
+  const $checkPwResult = document.querySelector('.sign-up__valid.pw');
 
-  const $signUpBtn = document.querySelector('.type--long:has(#signUpComplete)')
+  // 회원가입 버튼 노드
+  const $signUpBtn = document.querySelector('.type--long:has(#signUpComplete)');
 
+  // 별명, 이메일, 비밀번호 인증 상태 초기값
   let isNicknameValidated = false;
   let isEmailValidated = false;
   let isPwValidated = false;
 
+  // 별명 중복확인 버튼 클릭 이벤트 발생 시 실행 코드
   $checkNicknameBtn.addEventListener('click', function () {
     const userNickname = $signUpNickname.value;
+    // 별명 입력 확인 알고리즘
     if (userNickname) {
+      // 별명이 입력된 경우, 별도 중복 확인 없이 사용 가능한 닉네임 등록 -> API 여부 확인
       $checkNicknameResult.textContent = '사용할 수 있는 닉네임입니다.';
       $checkNicknameResult.classList.remove('invalid');
+
+      // 별명 인증 상태 변경
       isNicknameValidated = true;
     } else {
+      // 별명이 입력되지 않은 경우, 버튼 클릭 시 안내 메시지 출력
       $checkNicknameResult.textContent = '닉네임을 입력해주세요.';
       $checkNicknameResult.classList.add('invalid');
+
+      //별명이 입력되지 않은 경우 인증 상태는 false
       isNicknameValidated = false;
     }
   })
 
+  // 이메일 중복확인 버튼 클릭 이벤트 발생 시 실행 코드
   $checkEmailBtn.addEventListener('click', function () {
+    // 이메일 정규 표현식 지정
     const regExEmail = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     const userEmail = $signUpEmail.value;
     if (userEmail) {
       if (userEmail.match(regExEmail)) {
-        console.log('유효한 이메일입니다.')
+        // 올바른 형식의 이메일이 입력된 경우, 아래 코드 실행
+        // 이메일 중복확인 API 활용
         axios({
           method: 'get',
           url: '/api/users/email',
@@ -48,85 +64,112 @@ window.addEventListener('load', () => {
             'content-type': 'application/json',
             accept: 'application/json'
           },
+          // 이메일 입력란에 입력된 데이터 서버 전송
           params: {
             email: `${userEmail}`,
           }
         }).then(response => {
+          // 중복된 이메일 없는 경우, 사용 가능 이메일 안내 메시지 출력
           $checkEmailResult.textContent = '사용할 수 있는 이메일입니다.';
           $checkEmailResult.classList.remove('invalid');
+          // 이메일 확인 상태값 true 로 변경
+          isEmailValidated = true;
         }).catch(error => {
           if (error.response.status === 409) {
+            // 중복된 이메일이 존재하는 경우, 에러 메시지 출력
             const invalidEmail = error.response.data.message;
             $checkEmailResult.textContent = invalidEmail;
             $checkEmailResult.classList.add('invalid');
+            // 이메일 확인 상태값 false
+            isEmailValidated = false;
           }
         })
       } else {
+        // 올바르지 않은 형식의 이메일이 입력된 경우, 안내 메시지 출력
         console.log('유효하지 않은 이메일 형식입니다.');
         $checkEmailResult.textContent = '유효하지 않은 이메일 형식입니다';
         $checkEmailResult.classList.add('invalid');
       }
-      console.log(userEmail);
-      isEmailValidated = true;
+
     } else {
+      // 이메일 입력란에 입력값 없는 경우 안내 메시지 출력
       $checkEmailResult.textContent = '이메일을 입력해주세요.'
       $checkEmailResult.classList.add('invalid');
+      // 이메일 확인 상태값 false 
       isEmailValidated = false;
     }
   });
 
+  // 비밀번호 입력 이벤트 발생 시 실행 코드
   $signUpPw.addEventListener('input', function () {
     let userPw = $signUpPw.value;
+    // 비밀번호 정규 표현식, 최소 8자 이상, 숫자와 문자는 필수 입력, 특수문자, 대소문자 선택
     const regExPw = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d\W]{8,}$/;
+    // 비밀번호가 정규 표현식의 조건 만족 시
     if (regExPw.test(userPw)) {
       $checkPwResult.classList.add('hidden');
+
+      // 비밀번호 확인란 입력 시 발생 이벤트
       $signUpPwCheck.addEventListener('input', function () {
-        let userPw = $signUpPw.value;
         const userPwCheck = $signUpPwCheck.value;
         if (userPwCheck !== userPw) {
+          // 비밀번호와 비밀번호 확인란 입력값 일치하지 않는 경우, 안내 메시지 출력
           $checkPwResult.textContent = '비밀번호가 일치하지 않습니다.'
           $checkPwResult.classList.remove('hidden');
           $checkPwResult.classList.add('invalid');
+          // 비밀번호 확인 상태값 false
           isPwValidated = false;
         } else {
+          // 비밀번호와 비밀번화 확인란 입력값 일치 시, 안내 메시지 출력
           $checkPwResult.textContent = '비밀번호가 일치합니다.'
           $checkPwResult.classList.remove('hidden');
           $checkPwResult.classList.remove('invalid');
+          // 비밀번호 확인 상태값 true
           isPwValidated = true;
         }
       })
     } else {
+      // 비밀번호 입력값이 정규 표현식의 조건을 만족하지 않는 경우, 안내 메시지 출력
       $checkPwResult.textContent = '대소문자, 숫자 조합 8자 이상이어야 합니다.';
       $checkPwResult.classList.remove('hidden');
       $checkPwResult.classList.add('invalid');
+      // 비밀번호 확인 상태값 false
       isPwValidated = false;
     };
   })
 
+  // 회원가입 버튼 활성화 코드
   window.addEventListener('input', function () {
     if (isNicknameValidated && isEmailValidated && isPwValidated) {
+      // 별명, 이메일, 비밀번호 확인 상태값이 모두 true 인 경우, 버튼 활성화 상태로 변경
       $signUpBtn.classList.add('active');
     } else {
+      // 별명, 이메일, 비밀번호 확인 상태값 중 하나라도 false 인 경우, 버튼 비활성화 상태로 변경
       $signUpBtn.classList.remove('active');
     }
   })
 
+  // 회원가입 버튼 클릭 이벤트 발생 시 실행 코드
   $signUpBtn.addEventListener('click', function (e) {
     e.preventDefault();
     if (!isNicknameValidated && !isEmailValidated) {
+      // 별명 및 이메일 중복확인 미실행 시
       if (isNicknameValidated === false) {
+        // 별명 중복확인 미실행 시, 안내 메시지 출력, 해당 입력란에 focus
         $checkNicknameResult.textContent = '닉네임 중복확인을 진행해주세요.';
         $checkNicknameResult.classList.remove('hidden');
         $checkNicknameResult.classList.add('invalid');
         $signUpNickname.focus();
       }
       if (isEmailValidated === false) {
+        // 별명 중복확인 미실행 시, 안내 메시지 출력, 해당 입력란에 focus
         $checkEmailResult.textContent = '이메일 중복확인을 진행해주세요.'
         $checkEmailResult.classList.remove('hidden');
         $checkEmailResult.classList.add('invalid');
         $signUpEmail.focus();
       }
     } else {
+      // 전체 중복확인 실행 및 모든 입력란에 정확한 형식의 데이터가 입력되었을 경우, 서버에 request 전송
       const signUpNickname = $signUpNickname.value;
       const signUpEmail = $signUpEmail.value;
       const signUpPw = $signUpPw.value;
@@ -134,11 +177,13 @@ window.addEventListener('load', () => {
       axios({
         method: 'post',
         url: '/api/users',
+        // 요청 헤더
         headers: {
           'client-id': 'vanilla03',
           'content-type': 'application/json',
           accept: 'application/json'
         },
+        // 전송 데이터
         data: {
           email: `${signUpEmail}`,
           password: `${signUpPw}`,
@@ -148,10 +193,14 @@ window.addEventListener('load', () => {
         }
       })
         .then(response => {
+          // 회원가입 성공 시, 시작하기 메인 화면으로 이동
           console.log(response);
+          window.open('/src/features/start/start.html', '_self');
         })
         .catch(error => {
+          // 회원가입 실패 시(500 에러 발생)
           if (error.response.status === 500) {
+            // alert 로 에러 메시지 출력
             alert(error.response.message);
           }
         })
