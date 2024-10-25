c60a46791dfbb276bc901c02f039526aba01355d
okang1014 <okang1014@gmail.com>
2024-10-25 14:50:02 +0900

    feat: 시작하기 회원가입 기능 추가

--- src/features/start/signUp.js
+++ src/features/start/signUp.js
@@ -15,14 +15,13 @@ window.addEventListener('load', () => {
   const $signUpPwCheck = document.getElementById('signUpPwCheck');
   const $checkPwResult = document.querySelector('.sign-up__valid.pw')
 
-  const $signUpBtn = document.getElementById('signUpComplete')
+  const $signUpBtn = document.querySelector('.type--long:has(#signUpComplete)')
 
   let isNicknameValidated = false;
   let isEmailValidated = false;
   let isPwValidated = false;
 
   $checkNicknameBtn.addEventListener('click', function () {
-    console.log('check nickname button clicked');
     const userNickname = $signUpNickname.value;
     if (userNickname) {
       $checkNicknameResult.textContent = '사용할 수 있는 닉네임입니다.';
@@ -36,7 +35,6 @@ window.addEventListener('load', () => {
   })
 
   $checkEmailBtn.addEventListener('click', function () {
-    console.log('check email button clicked');
     const regExEmail = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     const userEmail = $signUpEmail.value;
     if (userEmail) {
@@ -77,46 +75,86 @@ window.addEventListener('load', () => {
     }
   });
 
-  $signUpPw.addEventListener('change', function () {
-    const userPw = $signUpPw.value;
+  $signUpPw.addEventListener('input', function () {
+    let userPw = $signUpPw.value;
     const regExPw = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d\W]{8,}$/;
     if (regExPw.test(userPw)) {
-      console.log('valid password');
       $checkPwResult.classList.add('hidden');
+      $signUpPwCheck.addEventListener('input', function () {
+        let userPw = $signUpPw.value;
+        const userPwCheck = $signUpPwCheck.value;
+        if (userPwCheck !== userPw) {
+          $checkPwResult.textContent = '비밀번호가 일치하지 않습니다.'
+          $checkPwResult.classList.remove('hidden');
+          $checkPwResult.classList.add('invalid');
+          isPwValidated = false;
+        } else {
+          $checkPwResult.textContent = '비밀번호가 일치합니다.'
+          $checkPwResult.classList.remove('hidden');
+          $checkPwResult.classList.remove('invalid');
+          isPwValidated = true;
+        }
+      })
     } else {
       $checkPwResult.textContent = '대소문자, 숫자 조합 8자 이상이어야 합니다.';
       $checkPwResult.classList.remove('hidden');
+      $checkPwResult.classList.add('invalid');
+      isPwValidated = false;
     };
   })
-  $signUpPwCheck.addEventListener('change', function () {
-    const userPw = $signUpPw.value;
-    const userPwCheck = $signUpPwCheck.value;
-    if (userPwCheck !== userPw) {
-      $checkPwResult.textContent = '비밀번호가 일치하지 않습니다.'
-      $checkPwResult.classList.remove('hidden');
-      $checkPwResult.classList.add('invalid');
-    } else {
-      $checkPwResult.textContent = '비밀번호가 일치합니다.'
-      $checkPwResult.classList.remove('hidden');
-      $checkPwResult.classList.remove('invalid');
-      isPwValidated = true;
-    }
-  })
 
-  window.addEventListener('change', function () {
+  window.addEventListener('input', function () {
     if (isNicknameValidated && isEmailValidated && isPwValidated) {
       $signUpBtn.classList.add('active');
+    } else {
+      $signUpBtn.classList.remove('active');
     }
   })
 
   $signUpBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('sign-up complete button clicked');
-    if (isNicknameValidated && isEmailValidated) {
-      console.log($signUpNickname.value, $signUpEmail.value);
+    if (!isNicknameValidated && !isEmailValidated) {
+      if (isNicknameValidated === false) {
+        $checkNicknameResult.textContent = '닉네임 중복확인을 진행해주세요.';
+        $checkNicknameResult.classList.remove('hidden');
+        $checkNicknameResult.classList.add('invalid');
+        $signUpNickname.focus();
+      }
+      if (isEmailValidated === false) {
+        $checkEmailResult.textContent = '이메일 중복확인을 진행해주세요.'
+        $checkEmailResult.classList.remove('hidden');
+        $checkEmailResult.classList.add('invalid');
+        $signUpEmail.focus();
+      }
     } else {
-      console.log('닉네임과 이메일 중복확인을 진행해주세요')
+      const signUpNickname = $signUpNickname.value;
+      const signUpEmail = $signUpEmail.value;
+      const signUpPw = $signUpPw.value;
+      console.log($signUpNickname.value, $signUpEmail.value);
+      axios({
+        method: 'post',
+        url: '/api/users',
+        headers: {
+          'client-id': 'vanilla03',
+          'content-type': 'application/json',
+          accept: 'application/json'
+        },
+        data: {
+          email: `${signUpEmail}`,
+          password: `${signUpPw}`,
+          name: `${signUpNickname}`,
+          type: 'user',
+          image: '/src/assets/images/no_profile.svg'
+        }
+      })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          if (error.response.status === 500) {
+            alert(error.response.message);
+          }
+        })
     }
   })
-
 })
