52e22f9ef6cfb90219616a4bed9f049142cf7697
okang1014 <okang1014@gmail.com>
2024-10-27 14:47:55 +0900

    feat: 글쓰기 페이지 게시글 저장 기능 추가

--- src/features/write/write.js
+++ src/features/write/write.js
@@ -6,44 +6,93 @@ window.addEventListener('load', function () {
 
   //로그인되지 않은 경우(세션 스토리지에 로그인 상태 속성 없는 경우), 시작하기 페이지로 이동
   if (!sessionStorage.getItem('login-status')) {
+    window.alert('로그인이 필요한 서비스입니다.');
     window.open('/src/features/start/start.html', '_self');
   }
 
   const $postTitle = document.getElementById('postTitle');
   const $postSubtitle = document.getElementById('postSubtitle');
   const $postContent = document.getElementById('postContent');
+
+  const $postCancel = document.getElementById('postCancel');
   const $postSave = document.getElementById('postSave');
+  const $uploadFile = document.getElementById('uploadFile');
+
+  $postCancel.addEventListener('click', function () {
+    console.log('cancel');
+    if ($postTitle.value || $postSubtitle.value || $postCancel.value) {
+      const cancel = confirm('작성 중인 내용이 있습니다. 작성을 종료하시겠습니까?');
+      if (cancel) {
+        history.back();
+      }
+    }
+  })
+
+  // window.addEventListener('chang')
+
+  window.addEventListener('input', function () {
+    if ($postTitle.value && $postSubtitle.value && $postContent.value) {
+      document.querySelector('.active').classList.remove('hidden');
+      document.querySelector('.disabled').classList.add('hidden');
+      $postSave.classList.add('confirmSave');
+    } else {
+      document.querySelector('.active').classList.add('hidden');
+      document.querySelector('.disabled').classList.remove('hidden');
+      $postSave.classList.remove('confirmSave');
+    }
+  })
 
   $postSave.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('post saved');
-    console.log($postTitle.value);
-    console.log($postSubtitle.value);
-    console.log($postContent.value);
+    const title = $postTitle.value;
+    const subtitle = $postSubtitle.value;
+    const content = $postContent.value;
+    const userIdNum = sessionStorage.getItem('login-user-ID-Num');
+    const userName = sessionStorage.getItem('login-user-name');
 
-    let userName;
-    let userIdNum;
-    let userData = [userName, userIdNum];
+    if (!$postSave.classList.item('confirmSave')) {
+      console.log('disabled');
+    } else {
+      const save = confirm('저장하시겠습니까?');
+      if (save) {
+        axios({
+          method: 'post',
+          url: '/api/posts',
+          headers: {
+            'client-id': 'vanilla03',
+            'Content-Type': 'application/json',
+            accept: 'application/json'
+          },
+          data: {
+            'type': 'info',
+            'title': `${title}`,
+            'extra': {
+              'subTitle': `${subtitle}`
+            },
+            user: {
+              '_id': `${userIdNum}`,
+              'name': `${userName}`
+            },
+            'content': `${content}`,
+            // image: 
+          }
+        }).then(response => {
+          alert('저장을 완료했습니다');
+        }).catch(error => {
+          if (error.response.status === 500) {
+            const errorMsg = error.response.data.message;
+            alert(errorMsg);
+          }
+        })
 
-    axios({
-      method: 'get',
-      url: `/api/users?email=${sessionStorage.getItem('userEmail')}`,
-      headers: {
-        'client-id': 'vanilla03',
-        'content-type': 'application/json',
-        accept: 'application/json',
+        console.log($postTitle.value);
+        console.log($postSubtitle.value);
+        console.log($postContent.value);
+      } else {
+        alert('저장을 취소하였습니다')
       }
-    })
-      .then(response => {
-        let userName = response.data.item[0].name;
-        let userIdNum = response.data.item[0]._id;
-        let userData = [userName, userIdNum];
-        console.log(userData);
-        return userData
-      })
-      .catch(error => console.log(error.response.data));
+    }
 
-    console.log(userData);
   })
 
 
