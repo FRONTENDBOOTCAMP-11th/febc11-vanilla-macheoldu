f7d248fc5bb85f0b2df9937f725d724076245c1d
okang1014 <okang1014@gmail.com>
2024-10-29 01:48:23 +0900

    config: vite config 설정 추가(각 페이지의 js, css)

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,54 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
+        // home 
         home: resolve(__dirname, 'src/features/home/home.html'),
+        homeJS: resolve(__dirname, 'src/features/home/home.js'),
+        homeCSS: resolve(__dirname, 'src/features/home/home.css'),
+
+        // discover
         discover: resolve(__dirname, 'src/features/discover/discover.html'),
+        discoverJS: resolve(__dirname, 'src/features/discover/discover.js'),
+        discoverCSS: resolve(__dirname, 'src/features/discover/discover.css'),
+
+        // write
         write: resolve(__dirname, 'src/features/write/write.html'),
+        writeJS: resolve(__dirname, 'src/features/write/write.js'),
+        writeCSS: resolve(__dirname, 'src/features/write/write.css'),
+
+        // myBox
         myBox: resolve(__dirname, 'src/features/myBox/myBox.html'),
+        myBoxJS: resolve(__dirname, 'src/features/myBox/myBox.js'),
+        myBoxCSS: resolve(__dirname, 'src/features/myBox/myBox.css'),
+
+        // author
         author: resolve(__dirname, 'src/features/author/author.html'),
+        authorJS: resolve(__dirname, 'src/features/author/author.js'),
+        authorCSS: resolve(__dirname, 'src/features/author/author.css'),
+
+        // detail
         detail: resolve(__dirname, 'src/features/detail/detail.html'),
+        detailJS: resolve(__dirname, 'src/features/detail/detail.js'),
+        detailCSS: resolve(__dirname, 'src/features/detail/detail.css'),
+
+        // start
+        // start > main
         start: resolve(__dirname, 'src/features/start/start.html'),
+        startJS: resolve(__dirname, 'src/features/start/start.js'),
+        startCSS: resolve(__dirname, 'src/features/start/start.css'),
+        // start > signUp
         signUp: resolve(__dirname, 'src/features/start/signUp.html'),
-        startKakao: resolve(__dirname, 'src/features/start/start-kakao.html'),
-        loginKakao: resolve(__dirname, 'src/features/start/login-kakao.html'),
-        topMenu: resolve(
-          __dirname,
-          'src/features/components/top-menu/top-menu.html',
-        ),
-        navigation: resolve(
-          __dirname,
-          'src/features/components/navigation/navigation.html',
-        ),
+        signUpJS: resolve(__dirname, 'src/features/start/signUp.js'),
+
+        // components
+        // components > top-menu
+        topMenu: resolve(__dirname, 'src/features/components/top-menu/top-menu.html'),
+        topMenuJS: resolve(__dirname, 'src/features/components/top-menu/top-menu.js'),
+        topMenuCSS: resolve(__dirname, 'src/features/components/top-menu/top-menu.css'),
+        // components > navigation
+        navigation: resolve(__dirname, 'src/features/components/navigation/navigation.html'),
+        navigationJS: resolve(__dirname, 'src/features/components/navigation/navigation.js'),
+        navigationCSS: resolve(__dirname, 'src/features/components/navigation/navigation.css'),
       },
     },
   },
