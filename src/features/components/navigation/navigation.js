0dce65bd2b0a658893759747087ed840cdc5cada
okang1014 <okang1014@gmail.com>
2024-10-28 10:00:31 +0900

    feat: 공통통컴포넌트 js 파일 수정, 현재 위치 페이지에 따라 버튼 표시 변경 기능 추가

--- src/features/components/navigation/navigation.js
+++ src/features/components/navigation/navigation.js
@@ -2,4 +2,14 @@ fetch('/src/features/components/navigation/navigation.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('navigation').innerHTML = data;
+
+    // 현재 위치한 페이지에 따라 버튼이 변경되어 출력
+    const currentPage = window.location.pathname;
+    const page = currentPage.split("/").pop().replace(".html", "");
+    document.querySelectorAll("nav a").forEach(anchor => {
+      if (anchor.getAttribute("data-page") === page) {
+        anchor.querySelector('figure .default').classList.add('hidden');
+        anchor.querySelector('figure .current').classList.remove('hidden');
+      }
+    })
   });
