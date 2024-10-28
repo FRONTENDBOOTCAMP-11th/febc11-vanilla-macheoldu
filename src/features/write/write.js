d7efa287961a6afbafbfaf7d8a686ace697e8943
okang1014 <okang1014@gmail.com>
2024-10-28 16:31:45 +0900

    feat: 글쓰기 페이지 기능 구현 작업 완료(Issue #64 해결)

--- src/features/write/write.js
+++ src/features/write/write.js
@@ -10,42 +10,59 @@ window.addEventListener('load', function () {
     window.open('/src/features/start/start.html', '_self');
   }
 
+  // 필요 노드 획득
+  // 글쓰기 내용 영역
   const $postTitle = document.getElementById('postTitle');
   const $postSubtitle = document.getElementById('postSubtitle');
   const $postContent = document.getElementById('postContent');
 
+  // 글쓰기 상단바 영역
   const $postCancel = document.getElementById('postCancel');
   const $postSave = document.getElementById('postSave');
 
+  // 글쓰기 하단바 영역
+  // 글쓰기 파일 첨부 버튼
   const $uploadFile = document.getElementById('uploadFile');
+  // 선택한 파일 표시 리스트
   const $fileList = document.querySelector('.file-list');
 
+  // 글쓰기 상단바 취소 클릭 시 이벤트
   $postCancel.addEventListener('click', function () {
+    // 작성 중인 내용이 있는 경우 Confirm dialog 출력
     if ($postTitle.value || $postSubtitle.value || $postCancel.value) {
-      const cancel = confirm('작성 중인 내용이 있습니다. 작성을 종료하시겠습니까?');
-      if (cancel) {
+      const confirmCancel = confirm('작성 중인 내용이 있습니다. 작성을 종료하시겠습니까?');
+      // 확인 선택 시 글쓰기 취소, 이전 페이지로 이동
+      if (confirmCancel) {
         history.back();
       }
     } else {
+      // 작성 중인 내용이 없는 경우 바로 이전 페이지로 이동
       history.back();
     }
   })
 
+  // 글쓰기 내용 추가 시 글쓰기 상단바 저장 버튼 활성화
   window.addEventListener('input', function () {
     if ($postTitle.value && $postSubtitle.value && $postContent.value) {
+      // 제목, 소제목, 내용란이 모두 입력된 경우, 저장 버튼 활성화
       document.querySelector('.active').classList.remove('hidden');
       document.querySelector('.disabled').classList.add('hidden');
       $postSave.classList.add('confirmSave');
     } else {
+      // 모두 입력되지 않은 경우, 저장 버튼 비활성화
       document.querySelector('.active').classList.add('hidden');
       document.querySelector('.disabled').classList.remove('hidden');
       $postSave.classList.remove('confirmSave');
     }
   })
 
+  // 선택한 파일을 하단에 표시
   $uploadFile.addEventListener('change', function (e) {
+    // 선택한 파일을 배열로 표시
     const files = Array.from(e.target.files);
+    // 파일 리스트 표시
     $fileList.classList.remove('hidden');
+    // 배열 내의 파일 데이터 파일 리스트에 출력
     files.forEach(function (file, index) {
       if (file.type.startsWith('image/')) {
         const reader = new FileReader();
@@ -58,81 +75,98 @@ window.addEventListener('load', function () {
         }
         reader.readAsDataURL(file);
       }
-      window.selectedFiles = files;
     })
   })
 
+  // 저장 버튼 클릭 이벤트 발생 시 실행 코드
   $postSave.addEventListener('click', function (e) {
     e.preventDefault();
-    // const title = $postTitle.value;
-    // const subtitle = $postSubtitle.value;
-    // const content = $postContent.value;
-    // const userAccessToken = sessionStorage.getItem('userAccessToken');
-    // const userRefreshToken = sessionStorage.getItem('userRefreshToken');
 
-    const selectedFiles = window.selectedFiles
-    console.dir(selectedFiles);
+    // 입력된 데이터 획득
+    const title = $postTitle.value;
+    const subtitle = $postSubtitle.value;
+    const content = $postContent.value;
+    const userAccessToken = sessionStorage.getItem('userAccessToken');
 
+    // 업로드된 파일을 FormData 에 추가
+    const selectedFiles = Array.from($uploadFile.files);
+    console.log(selectedFiles)
+    const imageFormData = new FormData();
+
+    selectedFiles.forEach((file) => {
+      imageFormData.append(`attach`, file);
+    })
+
+    // 저장 버튼 선택 시 Confirm dialog 출력
     if ($postSave.classList.item('confirmSave')) {
       const save = confirm('저장하시겠습니까?');
+      // 확인 선택 시
       if (save) {
+        // 첨부 파일 서버로 전송
         axios({
           method: 'post',
           url: '/api/files',
+          // 요청 헤더
           headers: {
             'client-id': 'vanilla03',
+            // multipart/form-data 로 요청
             'Content-Type': 'multipart/form-data',
-            // accept: 'application/json'
+            accept: 'application/json'
           },
-          attach: selectedFiles
-        }).then(response => {
-          console.dir(selectedFiles);
-          console.log(response.data);
-        }).catch(error => {
-          console.log(error.response.data.message);
+          data: imageFormData
         })
-        // const formData = new FormData();
-        // formData.append('type', 'info');
-        // formData.append('title', `${title}`);
-        // formData.append('extra[subTitle]', `${subtitle}`);
-        // formData.append('content', `${content}`);
+          // 요청 성공 시 실행 코드
+          .then(response => {
+            console.log(response.data);
+            const imgPath = [];
+            // 파일 데이터에서 이미지 url만 배열에 저장
+            for (let i = 0; i < response.data.item.length; i++) {
+              imgPath.push(response.data.item[i].path);
+            }
 
-        // if (window.selectedFiles && window.selectedFiles.length > 0) {
-        //   for (let i = 0; i < window.selectedFiles.length; i++) {
-        //     formData.append('image', window.selectedFiles[i]);
-        //   }
-        // }
-
-        // axios({
-        //   method: 'post',
-        //   url: '/api/posts',
-        //   headers: {
-        //     'client-id': 'vanilla03',
-        //     'Content-Type': 'application/json',
-        //     accept: 'application/json',
-        //     'Authorization': `Bearer ${userAccessToken}`
-        //   },
-        //   data: formData
-        // }).then(response => {
-        //   alert('저장을 완료했습니다');
-        //   console.log(window.selectedFiles);
-        // }).catch(error => {
-        //   if (error.response.status === 500) {
-        //     const errorMsg = error.response.data.message;
-        //     alert(errorMsg);
-        //   }
-        // })
-
-        // console.log($postTitle.value);
-        // console.log($postSubtitle.value);
-        // console.log($postContent.value);
-        // console.log(formData);
+            // 서버 요청 전송
+            axios({
+              method: 'post',
+              url: '/api/posts',
+              // 요청 header
+              headers: {
+                'client-id': 'vanilla03',
+                'Content-Type': 'application/json',
+                accept: 'application/json',
+                // 사용자 정보 추가
+                'Authorization': `Bearer ${userAccessToken}`
+              },
+              // 요청 body
+              data: {
+                type: 'info',
+                title: `${title}`,
+                extra: {
+                  subTitle: `${subtitle}`
+                },
+                content: `${content}`,
+                // 이미지가 포함된 배열을 image 필드로 전달
+                image: imgPath
+              }
+            }).then(response => {
+              // 요청 성공 시 안내 alert 출력, 홈 페이지로 이동
+              alert('저장을 완료했습니다');
+              window.open('/src/features/home/home.html')
+            }).catch(error => {
+              // 서버 에러(500) 출력 메시지
+              if (error.response.status === 500) {
+                const errorMsg = error.response.data.message;
+                alert(errorMsg);
+              }
+            })
+          }).catch(error => {
+            // 파일 업로드 실페 시 안내 메시지
+            alert(error.response.data.message);
+          })
       } else {
+        // 저장 확인 confirm dialog 에서 취소 선택 시, 이동 없이 현재 페이지 유지
         alert('저장을 취소하였습니다')
       }
     }
-
   })
-
 
 })
